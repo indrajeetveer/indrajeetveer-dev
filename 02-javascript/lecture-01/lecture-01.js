79a5672c84b2d9b462e29277bfd15395bb05d204
Indrajeet Veer <veerindrajeet004@gmail.com>
2026-07-13 23:42:10 +0530

    JS L01 Begining

--- 02-javascript/lecture-01/lecture-01.js
+++ 02-javascript/lecture-01/lecture-01.js
@@ -1,2 +1,22 @@
 console.log("My name is : Indrajeet Veer , MCA Student");
 console.log("Start Javascript");
+
+var a = 20;
+let b = 30;
+const c = 40;
+
+console.log(a,b,c);
+
+// c = 50 ;   This is not allowed you can not assign the new value to an constant
+
+
+// Printing statement 
+
+console.log("Learn Javascript");
+console.error("This is an error message");
+console.warn("This is an warning message");
+
+alert("Learn the javascript");
+
+let user = prompt("Enter your name");
+console.log('Hi'+ user)
