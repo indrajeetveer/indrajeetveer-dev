6e1d43a322dd98437f4866b6d51871800f700774
indrajeetveer <indrajeetveer1@gmail.com>
2026-07-22 23:54:11 +0530

    JS L12 More Dom

---
+++ 02-javascript/lecture-12/script.js
@@ -0,0 +1,25 @@
+// let h1 = document.querySelector('h1');
+// let btn = document.querySelector('button');
+
+
+// btn.addEventListener('click',()=>{
+//     h1.innerHTML = "User is Change...."
+
+//     setTimeout(()=>{
+//         h1.innerHTML = "Hello I an Indrajeet"
+//     },2000)
+// })
+
+
+// // setInterval(()=>{
+// //    alert("Session is complete")
+// // },3000)
+
+let btn = document.querySelector('button');
+let h2 = document.addEventListener('h2');
+
+let count = 0;
+
+btn.addEventListener('click',()=>{
+    
+})
