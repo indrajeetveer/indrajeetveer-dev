98ba7e4dcbb1c2ab8a33e56a9350b0dd2266ea98
indrajeetveer <indrajeetveer1@gmail.com>
2026-08-10 00:35:21 +0530

    JS Project Work

--- 02-javascript/JS_Project/script.js
+++ 02-javascript/JS_Project/script.js
@@ -22,7 +22,7 @@ let fullElemPageBackbtn = document.querySelectorAll(".fullElem .back");
 allElems.forEach((elem) => {
     elem.addEventListener("click", () => {
         let index = Number(elem.id);
-        fullElemPage[index].style.display = "block";
+        fullElemPage[index].style.display = "block";dd
     });
 });
 
@@ -35,3 +35,46 @@ fullElemPageBackbtn.forEach((back) => {
 }
 
 openCards();
+
+
+let h1 = document.querySelector(".heading")
+async function info() {
+    try {
+        let raw = await fetch("https://dummyjson.com/quotes/random");
+        let realdata = await raw.json();
+        h1.innerHTML = `"${realdata.quote}"`
+    } catch (error) {
+        console.log("Error:", error);
+    }
+}
+info();
+
+let userinput = document.querySelector('input');
+let btn = document.querySelector('#btn1');
+let result = document.querySelector('#result');
+
+btn.addEventListener("click", () => {
+
+    let input = userinput.value;
+
+    // Get old tasks from localStorage
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    // Add new task to array
+    tasks.push(input);
+
+    // Save updated array to localStorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    // Create heading
+    const heading = document.createElement("h3");
+    heading.innerHTML = input;
+    heading.className = "task";
+    heading.style.fontSize = "15px";
+
+    // Show task on page
+    result.append(heading);
+
+    // Clear input
+    userinput.value = "";
+});
