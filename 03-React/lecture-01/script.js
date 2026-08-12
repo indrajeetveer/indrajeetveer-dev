ccce2d280362c87e80ab7ff0f9c13d941c0a46ca
indrajeetveer <indrajeetveer1@gmail.com>
2026-08-12 23:55:33 +0530

    React L01 Create React Element

---
+++ 03-React/lecture-01/script.js
@@ -0,0 +1,12 @@
+// let h1 = document.createElement('h1');
+// h1.innerHTML = "Hello form Indrajeet";
+
+// document.body.appendChild(h1)
+
+let h1 = React.createElement('h1',null,'I am Learn the react');
+let h2 = React.createElement('h1',null,"I am indrajeet");
+
+let div = React.createElement('div',null,[h1,h2])
+
+let container = ReactDOM.createRoot(document.querySelector('#root'));
+container.render(div)
