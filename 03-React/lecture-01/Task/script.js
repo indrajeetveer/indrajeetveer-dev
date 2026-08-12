7b84c63ef289f837b4c01efd8f89305e1b7869f1
indrajeetveer <indrajeetveer1@gmail.com>
2026-08-13 01:04:41 +0530

    React L01 Task

---
+++ 03-React/lecture-01/Task/script.js
@@ -0,0 +1,10 @@
+let nav = React.createElement('nav',{calssName:'nav'},
+     //logo 
+     React.createElement(
+        'div',
+        {className:'logo'},
+        "*"
+     ),
+
+     
+)
--- 03-React/lecture-01/script.js
+++ 03-React/lecture-01/script.js
@@ -3,10 +3,18 @@
 
 // document.body.appendChild(h1)
 
-let h1 = React.createElement('h1',null,'I am Learn the react');
-let h2 = React.createElement('h1',null,"I am indrajeet");
+// let h1 = React.createElement('h1',null,'I am Learn the react');
+// let h2 = React.createElement('h1',null,"I am indrajeet");
 
-let div = React.createElement('div',null,[h1,h2])
+// let div = React.createElement('div',null,[h1,h2])
 
-let container = ReactDOM.createRoot(document.querySelector('#root'));
-container.render(div)
+// let container = ReactDOM.createRoot(document.querySelector('#root'));
+// container.render(div)
+
+
+function h1(){
+    return React.createElement('h1',{id:"veer"},'learn React');
+}
+
+let container = ReactDOM.createRoot(document.querySelector('#root'))
+container.render(h1())
