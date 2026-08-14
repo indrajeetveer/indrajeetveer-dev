685d3cfe8d91ddc99e1e30647d5ae839952e1cb9
indrajeetveer <indrajeetveer1@gmail.com>
2026-08-15 01:09:09 +0530

    React L03 Folder structure

---
+++ 03-React/lecture-03/Learn_React/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main id='mian'>
+        <div id='sub-div'>
+            <div id='profile'></div>
+            <h1>---Basic---</h1>
+            <h2>One Select Templet</h2>
+            <h2>100% Responsive Design</h2>
+            <h2>Credit remove permission</h2>
+            <button>Purches</button>
+        </div>
+    </main>
+  )
+}
+
+export default App
